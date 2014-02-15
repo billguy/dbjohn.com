f4e31cad76efb271610c87e5a76aa38b90ba96cc
David John <dj@dbjohn.com>
2014-02-15 08:44:31 -0600

    redirect old wordpress routes

--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,8 @@ DbjohnCom::Application.routes.draw do
   resources :slogans, except: [:show]
   resources :tags, only: [:index]
   resources :blogs
-  resources :contacts, only: [:new, :create], path: 'contact'
+  # redirect old wordpress blogs
+  get '/:year/:month/:day/:id' => redirect { |params, req| "/blogs/#{params[:id]}" }
 
   resources :pages
   get ':id', to: 'pages#show'

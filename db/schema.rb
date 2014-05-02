fa66da55e3d21beb6a65c2220c9969848bcbc1e4
David John <dj@dbjohn.com>
2014-05-02 16:50:38 -0500

    migrate acts_as_taggable

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140203161550) do
+ActiveRecord::Schema.define(version: 20140502214840) do
 
   create_table "blogs", force: true do |t|
     t.boolean  "published"
@@ -19,6 +19,17 @@ ActiveRecord::Schema.define(version: 20140203161550) do
     t.string   "permalink"
     t.text     "content"
     t.text     "javascript"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "categories", force: true do |t|
+    t.string   "title"
+    t.string   "permalink"
+    t.integer  "parent_id"
+    t.integer  "lft"
+    t.integer  "rgt"
+    t.integer  "depth"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
@@ -98,7 +109,8 @@ ActiveRecord::Schema.define(version: 20140203161550) do
   add_index "taggings", ["tag_id", "taggable_id", "taggable_type", "context", "tagger_id", "tagger_type"], name: "taggings_idx", unique: true
 
   create_table "tags", force: true do |t|
-    t.string "name"
+    t.string  "name"
+    t.integer "taggings_count", default: 0
   end
 
   add_index "tags", ["name"], name: "index_tags_on_name", unique: true
